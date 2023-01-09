8f9dbe0af3b23ace473671a8405c807a08cce0d4
vortana say <sayvortana.itc@gmail.com>
2023-01-09 05:49:13 -0600

    Initial Commit

---
+++ BetterRes/BetterResApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterResApp.swift
+//  BetterRes
+//
+//  Created by vsay on 1/9/23.
+//
+
+import SwiftUI
+
+@main
+struct BetterResApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BetterRes/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  BetterRes
+//
+//  Created by vsay on 1/9/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
